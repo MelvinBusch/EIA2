6504eebf4ae2c58e38273e12ed77294149967f05
melvin <melvin@macmelvin.eduroam.hs-furtwangen.de>
2018-03-22 13:30:38 +0100

    Aufgabe 00. Ein kleine interaktive Website

---
+++ Aufgabe00/main.js
@@ -0,0 +1,7 @@
+// Greetings!
+document.addEventListener("DOMContentLoaded", function () {
+    var name = prompt("Bitte Namen eingeben", "Name");
+    var container = document.getElementById("greetings");
+    container.innerHTML = "Hallo " + name + ". Wie geht's so?";
+});
+//# sourceMappingURL=main.js.map
