be43b49537466d2d0d1789278afd46e3948c4b93
MelvinBusch <melvin.busch@t-online.de>
2018-04-10 16:19:24 +0200

    Aufgabe 3 erste Version - unfertig

--- Aufgabe03/memory.js
+++ Aufgabe03/memory.js
@@ -1,2 +1,27 @@
-var baum = "Baum";
-var wald = "Wald";
+var Memory;
+(function(Memory) {
+  //const playerCount: string = prompt("Anzahl der Spieler eingeben:", "2 - 4 Spieler");
+  var cardPairs = 3; //prompt("Anzahl der Kartenpaare", "2 - 6 Kartenpaare");
+  var cards = [];
+  var words = [];
+  var MemoryCard = /** @class */ (function() {
+    function MemoryCard(id) {
+      this.id = id;
+    }
+    MemoryCard.prototype.init = function() {
+      var card = document.createElement("div");
+      cards.push(card);
+    };
+    MemoryCard.prototype.isTaken = function() {};
+    MemoryCard.prototype.isHidden = function() {};
+    MemoryCard.prototype.isOpen = function() {};
+    return MemoryCard;
+  }());
+  for (var i = 0; i < cardPairs * 2; i++) {
+    var card = new MemoryCard(i);
+    card.init();
+  }
+  console.log(cards);
+
+  function shuffleArray() {}
+})(Memory || (Memory = {}));
--- Aufgabe03/memory.ts
+++ Aufgabe03/memory.ts
@@ -1,2 +1,34 @@
-let baum : string = "Baum";
-let wald : string = "Wald";
+namespace Memory {
+  //const playerCount: string = prompt("Anzahl der Spieler eingeben:", "2 - 4 Spieler");
+  const cardPairs: number = 3; //prompt("Anzahl der Kartenpaare", "2 - 6 Kartenpaare");
+  let cards: HTMLElement[] = [];
+  let words: string[] = [];
+
+  class MemoryCard {
+
+    id: number;
+
+    constructor(id: number) {
+      this.id = id;
+    }
+    init() {
+      let card: HTMLElement = document.createElement("div");
+
+      cards.push(card);
+    }
+    isTaken() { }
+    isHidden() { }
+    isOpen() { }
+  }
+
+  for (let i: number = 0; i < cardPairs * 2; i++) {
+    let card: MemoryCard = new MemoryCard(i);
+    card.init();
+  }
+
+  console.log(cards);
+
+  function shuffleArray(): void {
+
+  }
+}
